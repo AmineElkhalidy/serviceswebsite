8eccde15181eafe6b7e3f8ba91b5ddeaaa293491
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-18 08:50:24 +0100

    Hero section added!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,11 @@ import { motion } from "framer-motion";
 import Header from "../components/Layout/Header/Header";
 
 // Location icon
-import { MapPinIcon } from "@heroicons/react/24/solid";
+import { MapPinIcon } from "@heroicons/react/24/outline";
+
+// Images
+import Devmain from "../assets/images/developping-main-color.svg";
+import Devsec from "../assets/images/developping-second-color.svg";
 
 const Home = () => {
   return (
@@ -15,52 +19,48 @@ const Home = () => {
       {/* Header component */}
       <Header />
 
-      <main className="overflow-hidden">
-        {/* Hero section */}
-        <section className="max-w-6xl mx auto px-6" id="hero">
+      {/* Main */}
+      <main>
+        <section className="min-h-[calc(100vh-5rem)] max-w-6xl mx-auto  flex justify-center items-center">
           <motion.div
             whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
             transition={{ duration: 0.5 }}
+            className="grid px-6 space-y-6  sm:grid-cols-2 sm:space-y-0 "
           >
-            <div>
-              <h1>
-                Hello,
-                <br />
-                I'm Amine Elkhalidy
+            <div className="space-y-4">
+              <span className="inline-flex items-center gap-2 font-medium md:text-lg">
+                <MapPinIcon className="h-5 w-5" /> Marrakech, Morocco
+              </span>
+
+              <h1 className="text-3xl font-thin md:text-4xl lg:text-5xl">
+                Hello, <br />
+                I'm <span>Amine Elkhalidy</span>
               </h1>
+
+              <p className="max-w-md text-lg text-gray-200 md:text-xl lg:text-2xl">
+                I am a{" "}
+                <span className="inline-block bg-blue text-gray-800">
+                  web developer
+                </span>
+                , I build things that lives on{" "}
+                <span className="inline-block bg-blue text-gray-800">
+                  the internet
+                </span>
+              </p>
             </div>
+
+            <img
+              className="justify-self-center w-[70%] md:w-[80%]"
+              src={Devmain}
+              alt="Developer developping an application"
+            />
           </motion.div>
         </section>
 
         {/* Services */}
-        <section className="" id="hero">
-          <motion.div
-            whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-            transition={{ duration: 0.5 }}
-          >
-            <div></div>
-          </motion.div>
-        </section>
 
         {/* Features */}
-        <section className="" id="hero">
-          <motion.div
-            whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-            transition={{ duration: 0.5 }}
-          >
-            <div></div>
-          </motion.div>
-        </section>
-
-        {/* Contact */}
-        <section className="" id="hero">
-          <motion.div
-            whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-            transition={{ duration: 0.5 }}
-          >
-            <div></div>
-          </motion.div>
-        </section>
+        <section className="h-screen bg-white" id="features"></section>
       </main>
     </React.Fragment>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,9 @@ module.exports = {
       colors: {
         main: "#00112c",
         second: "#38E8B0",
+        blue: "#1AAFD0",
+        cyan: "#17a2b8",
+        teal: "#1AAFD0",
       },
     },
   },
