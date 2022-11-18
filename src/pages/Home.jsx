adfcc4177c727096fa75a1a2255bf0340cade732
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-18 09:51:13 +0100

    Features section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 import Header from "../components/Layout/Header/Header";
 
 // Location icon
-import { MapPinIcon } from "@heroicons/react/24/outline";
+import { MapPinIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
 
 // Images
 import Devmain from "../assets/images/developping-main-color.svg";
@@ -60,7 +60,99 @@ const Home = () => {
         {/* Services */}
 
         {/* Features */}
-        <section className="h-screen bg-white" id="features"></section>
+        <section
+          className="min-h-screen bg-slate-100 sm:h-screen"
+          id="features"
+        >
+          <motion.div
+            whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+            transition={{ duration: 0.5 }}
+            className="max-w-6xl mx-auto h-full flex justify-center items-center px-6 md:px-0"
+          >
+            <div className="grid gap-12 sm:grid-cols-2 md:grid-cols-3">
+              {/* Feature 1 */}
+              <div className="text-center grid gap-4">
+                <img
+                  src="https://cdn1.dan.com/assets/public/flame-70467fdaea05848dd31989d3d69d8e40532ca4a367a35b2b4afb335f58d1cbaf.svg"
+                  alt="feature logo"
+                  className="justify-self-center"
+                />
+
+                <div className="text-gray space-y-1">
+                  <h3 className="text-main text-2xl font-semibold xl:text-3xl">
+                    High quality
+                  </h3>
+                  <p className="max-w-xs xl:text-lg">
+                    I will make the best application for you in terms of quality
+                  </p>
+                  <a
+                    href="#"
+                    className="inline-flex items-center gap-2 text-blue font-medium"
+                  >
+                    Visit my portfolio{" "}
+                    <span>
+                      <ArrowRightIcon className="h-4 w-4" />
+                    </span>
+                  </a>
+                </div>
+              </div>
+
+              {/* Feature 2 */}
+              <div className="text-center grid gap-4">
+                <img
+                  src="https://cdn2.dan.com/assets/public/cards-1496ee9a2a19fa194eec03d322f58599a5e67615507f7f16d11546dc612ab8a4.svg"
+                  alt="feature logo"
+                  className="justify-self-center"
+                />
+
+                <div className="text-gray space-y-1">
+                  <h3 className="text-main text-2xl font-semibold xl:text-3xl">
+                    Web Hosting
+                  </h3>
+                  <p className="max-w-xs xl:text-lg">
+                    I will make the best application for you in terms of quality
+                  </p>
+                  <a
+                    href="#"
+                    className="inline-flex items-center gap-2 text-blue font-medium"
+                  >
+                    Visit my portfolio{" "}
+                    <span>
+                      <ArrowRightIcon className="h-4 w-4" />
+                    </span>
+                  </a>
+                </div>
+              </div>
+
+              {/* Feature 3 */}
+              <div className="text-center grid gap-4">
+                <img
+                  src="https://cdn1.dan.com/assets/public/premium-59f1968fb4be2fde611d8aa97bbb39fac1fc5490ab14fd6d209427dfc527fe3e.svg"
+                  alt="feature logo"
+                  className="justify-self-center"
+                />
+
+                <div className="text-gray space-y-1">
+                  <h3 className="text-main text-2xl font-semibold xl:text-3xl">
+                    SEO
+                  </h3>
+                  <p className="max-w-xs xl:text-lg">
+                    I will make the best application for you in terms of quality
+                  </p>
+                  <a
+                    href="#"
+                    className="inline-flex items-center gap-2 text-blue font-medium"
+                  >
+                    Visit my portfolio{" "}
+                    <span>
+                      <ArrowRightIcon className="h-4 w-4" />
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        </section>
       </main>
     </React.Fragment>
   );
